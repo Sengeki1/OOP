f4f158813e6c1cd7f8dd12e08c7355e6fd3999ca
Sengeki1 <gamersengeki@gmail.com>
2023-06-16 16:21:21 -0100

    used __repr__ method

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 # The pay rate after 20% discount
+    all = [] # All Attributes
     def __init__(self, name: str, price: float, quantity = 0):
         #Run Validations to the received arguments
         assert price >= 0, f"Price {price} is not greater or equal than zero!"
@@ -10,17 +11,25 @@ class Item:
         self.priceVar = price
         self.quantityVar = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.priceVar * self.quantityVar
     
     def apply_discount(self):
         self.priceVar = self.priceVar * self.pay_rate # Overite the attribute of price
 
-item1 = Item("Phone", 100, 1)
-item1.apply_discount()
-print(item1.priceVar)
+    def __repr__(self):
+        return f"Item('{self.nameVar}', {self.priceVar}, {self.quantityVar})"
 
+# Instances 
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.priceVar)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
+for instance in Item.all:
+    print(instance.nameVar)
